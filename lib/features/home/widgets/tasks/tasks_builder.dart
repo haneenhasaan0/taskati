46638a7ee5b5688be54120b33b9a37b646af306c
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-03-12 15:37:56 +0200

    feat(tasks): add TaskCard widget and refactor TasksBuilder structure

--- lib/features/home/widgets/tasks/tasks_builder.dart
+++ lib/features/home/widgets/tasks/tasks_builder.dart
@@ -2,9 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:taskati/core/constants/app_images.dart';
 import 'package:taskati/core/models/task_model.dart';
 // import 'package:taskati/core/services/hive_helper.dart';
 import 'package:taskati/core/styles/colors.dart';
+import 'package:taskati/core/styles/text_styles.dart';
+import 'package:taskati/core/widgets/custom_svg_picture.dart';
+import 'package:taskati/features/home/widgets/tasks/task_card.dart';
 
 class TasksBuilder extends StatefulWidget {
   const TasksBuilder({super.key, required this.tasks});
@@ -30,15 +34,8 @@ class _TasksBuilderState extends State<TasksBuilder> {
       itemCount: widget.tasks.length,
 
       itemBuilder: (context, index) {
-        return Container(
-          height: 80,
-          decoration: BoxDecoration(
-            color: AppColors.accentColor,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          // child: Center(child: Text(tasks[index].title)),
-          child: Center(child: Text(widget.tasks[index].title)),
-        );
+        var task = widget.tasks[index];
+        return TaskCard(task: task);
       },
 
       separatorBuilder: (context, index) => Gap(12),
